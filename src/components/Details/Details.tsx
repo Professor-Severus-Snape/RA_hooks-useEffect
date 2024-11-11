2d7ffdd4114412da73e420338a934b417d484951
Julia Fokanova <oncologmma@mail.ru>
2024-11-11 15:32:57 +0300

    mentor's edits

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -9,9 +9,12 @@ const Details = ({ data }: IDetailsProps) => {
   const { avatar, name } = data;
   const { city, company, position } = data.details;
 
+  // чтобы заставить реакт менять аватар при одной и той же ссылке на "https://i.pravatar.cc/300":
+  const queryParam = `?key=${Math.random()}`;
+
   return (
     <div className="details">
-      <img className="details__avatar" src={avatar} alt="avatar" />
+      <img className="details__avatar" src={avatar + queryParam} alt="avatar" />
       <h3 className="details__description details__name">{name}</h3>
       <p className="details__description details__city">City: {city}</p>
       <p className="details__description details__company">Company: {company}</p>
